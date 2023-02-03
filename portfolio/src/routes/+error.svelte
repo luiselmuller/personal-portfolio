37ff09e147daa8162d528bc9b7cd7ad5af799ab4
luiselmuller <luiselmuller@gmail.com>
2023-02-03 00:01:40 -0400

    Delete portfolio directory

--- portfolio/src/routes/+error.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import { page } from '$app/stores';
-</script>
-
-<div class="w-screen h-screen flex justify-center items-center">
-    <h1 class="text-primary-hover text-2xl font-semibold">
-        {$page.status}: {$page.error?.message}
-    </h1>
-</div>
--- portfolio/src/routes/+layout.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-    // Import tailwindcss stuff
-    import "../app.css";
-</script>
-
-<slot></slot>
